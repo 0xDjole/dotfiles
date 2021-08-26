25d2d3e87d7d0a6308ef62e9708ebda842aa3942
Djole <djole.nikolic.priv@gmail.com>
2021-08-26 05:07:24 +0200

    ws 10 as helper

--- i3/load_layout.sh
+++ i3/load_layout.sh
@@ -8,7 +8,7 @@ i3-msg "workspace 1: Editor; append_layout ~/.config/i3/workspace_1.json"
 i3-msg "workspace 2: Terminal; append_layout ~/.config/i3/workspace_2.json"
 i3-msg "workspace 3: Browser; append_layout ~/.config/i3/workspace_3.json"
 i3-msg "workspace 4: Communication; append_layout ~/.config/i3/workspace_4.json"
-i3-msg "workspace 5: Helper; append_layout ~/.config/i3/workspace_5.json"
+i3-msg "workspace 10: Helper; append_layout ~/.config/i3/workspace_5.json"
 
 (alacritty --title Editor &)
 (alacritty --title Core &)
