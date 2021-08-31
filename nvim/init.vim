89399f55ed6ac4bc696e22801c883cb7234ca87b
Djole <djole.nikolic.priv@gmail.com>
2021-08-31 18:22:03 +0200

    lsp config updates

--- nvim/init.vim
+++ nvim/init.vim
@@ -86,6 +86,7 @@ nnoremap <silent>K :Lspsaga hover_doc<CR>
 inoremap <silent> <C-k> <Cmd>Lspsaga signature_help<CR>
 nnoremap <silent> gh <Cmd>Lspsaga lsp_finder<CR>
 nnoremap <silent> <C-j> :Lspsaga diagnostic_jump_next<CR>
+nnoremap <silent> ff    <cmd>lua vim.lsp.buf.formatting()<CR>
 
 " Searching shortcuts
 nnoremap <silent> <leader>p <cmd>Telescope find_files<cr>
--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -1,110 +1,88 @@
-local lsp = require('lspconfig')
-local coq = require "coq" 
+local nvim_lsp = require("lspconfig")
+local coq = require("coq")
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
+local format_async = function(err, _, result, _, bufnr)
+    if err ~= nil or result == nil then return end
+    if not vim.api.nvim_buf_get_option(bufnr, "modified") then
+        local view = vim.fn.winsaveview()
+        vim.lsp.util.apply_text_edits(result, bufnr)
+        vim.fn.winrestview(view)
+        if bufnr == vim.api.nvim_get_current_buf() then
+            vim.api.nvim_command("noautocmd :update")
+        end
+    end
+end
+vim.lsp.handlers["textDocument/formatting"] = format_async
+
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local buf_map = vim.api.nvim_buf_set_keymap
+    vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+    vim.cmd("command! LspOrganize lua lsp_organize_imports()")
+    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+    vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
+    vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
+    vim.cmd("command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
+    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
 
-   if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
+    buf_map(bufnr, "n", "gd", "lua vim.lsp.buf.definition()", {silent = true})
+    buf_map(bufnr, "n", "gr", "lua vim.lsp.buf.references()", {silent = true})
+    buf_map(bufnr, "n", "gt", "lua vim.lsp.buf.type_definition()", {silent = true})
+    buf_map(bufnr, "n", "K", "lua vim.lsp.buf.hover()", {silent = true})
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gt', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    if client.resolved_capabilities.document_formatting then
+        vim.api.nvim_exec([[
+         augroup LspAutocommands
+             autocmd! * <buffer>
+             autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting() 
+         augroup END
+         ]], true)
+    end
 end
 
-local saga = require 'lspsaga'
-saga.init_lsp_saga {
-  error_sign = '',
-  warn_sign = '',
-  hint_sign = '',
-  infor_sign = '',
-  border_style = "round",
+local filetypes = {
+    typescript = "eslint",
+    typescriptreact = "eslint",
 }
-
-local actions = require('telescope.actions')
-require('telescope').setup{
-  defaults = {
-    mappings = {
-      i = {
-        ["<esc>"] = actions.close
-      },
-    },
-  }
-}
-
-
-
-
-lsp.diagnosticls.setup {
-  on_attach = on_attach,
-  filetypes = { 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'svelte', 'css', 'less', 'scss', 'markdown', 'pandoc' },
-  init_options = {
-    linters = {
-      eslint = {
-        command = 'eslint_d',
-        rootPatterns = { '.git' },
+local linters = {
+    eslint = {
+        sourceName = "eslint",
+        command = "eslint_d",
+        rootPatterns = {".eslintrc.js", "package.json"},
         debounce = 100,
-        args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
-        sourceName = 'eslint_d',
+        args = {"--stdin", "--stdin-filename", "%filepath", "--format", "json"},
         parseJson = {
-          errorsRoot = '[0].messages',
-          line = 'line',
-          column = 'column',
-          endLine = 'endLine',
-          endColumn = 'endColumn',
-          message = '[eslint] ${message} [${ruleId}]',
-          security = 'severity'
+            errorsRoot = "[0].messages",
+            line = "line",
+            column = "column",
+            endLine = "endLine",
+            endColumn = "endColumn",
+            message = "${message} [${ruleId}]",
+            security = "severity"
         },
-        securities = {
-          [2] = 'error',
-          [1] = 'warning'
-        }
-      },
-    },
-    filetypes = {
-      javascript = 'eslint',
-      javascriptreact = 'eslint',
-      typescript = 'eslint',
-      typescriptreact = 'eslint',
-      svlete = 'eslint'
-    },
-    formatters = {
-      eslint_d = {
-        command = 'eslint_d',
-        args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
-        rootPatterns = { '.git' },
-      },
-      prettier = {
-        command = 'prettier',
-        args = { '--stdin-filepath', '%filename' }
-      }
-    },
-    formatFiletypes = {
-      css = 'prettier',
-      javascript = 'eslint_d',
-      javascriptreact = 'eslint_d',
-      json = 'prettier',
-      scss = 'prettier',
-      less = 'prettier',
-      typescript = 'eslint_d',
-      typescriptreact = 'eslint_d',
-      json = 'prettier',
-      markdown = 'prettier',
-      svelte = 'eslint_d'
+        securities = {[2] = "error", [1] = "warning"}
     }
-  }
 }
-
+local formatters = {
+    prettier = {command = "prettier", args = {"--stdin-filepath", "%filepath"}}
+}
+local formatFiletypes = {
+    typescript = "prettier",
+    typescriptreact = "prettier"
+}
+nvim_lsp.diagnosticls.setup {
+    on_attach = on_attach,
+    filetypes = vim.tbl_keys(filetypes),
+    init_options = {
+        filetypes = filetypes,
+        linters = linters,
+        formatters = formatters,
+        formatFiletypes = formatFiletypes
+    }
+}
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained",
@@ -114,8 +92,14 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
-lsp.tsserver.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
-lsp.svelte.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
-lsp.pyright.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
-lsp.tailwindcss.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
+nvim_lsp.tsserver.setup {
+    on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+        on_attach(client)
+    end
+}
+nvim_lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
+nvim_lsp.svelte.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
+nvim_lsp.pyright.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
+nvim_lsp.tailwindcss.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
+
