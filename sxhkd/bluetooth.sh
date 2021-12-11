cf4530570577a58a740e6cbb78830530e1945a26
Djole <djole.nikolic.priv@gmail.com>
2021-12-11 20:05:17 +0100

    bluetooth more

--- sxhkd/bluetooth.sh
+++ sxhkd/bluetooth.sh
@@ -1,2 +1,1 @@
-bluetoothctl power on && timeout 5 bluetoothctl scan on 
-bluetoothctl connect F8:AB:E5:17:F0:7C && sleep 5 && pacmd set-default-sink bluez_sink.F8_AB_E5_17_F0_7C.a2dp_sink
+bluetoothctl power on && bluetoothctl trust F8:AB:E5:17:F0:7C && bluetoothctl connect F8:AB:E5:17:F0:7C
