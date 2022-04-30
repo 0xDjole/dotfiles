b02b8dba4281453e45b093b58e5e23a47268f4b4
Djole <djole.nikolic.priv@gmail.com>
2022-04-30 04:10:01 +0200

    dont start the slack finally..

--- bspwm/start.sh
+++ bspwm/start.sh
@@ -3,5 +3,4 @@ xdo activate -N "Editor" || alacritty --class Editor &
 xdo activate -N "Terminal1" || alacritty --class Terminal1 &
 xdo activate -N "Terminal2" || alacritty --class Terminal2 &       
 xdo activate -N "Google-chrome" || google-chrome-stable &
-xdo activate -N "slack" || slack &
 xod activate -N "discord" || discord &
