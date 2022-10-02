eba0f9f03964201ba787ee0fb0ebb356171a84f2
Djole <0xdjole@gmail.com>
2022-10-02 17:04:02 +0200

    fuck chrome

--- bspwm/start.sh
+++ bspwm/start.sh
@@ -2,5 +2,5 @@ xdo kill -N "Alacritty"
 xdo activate -N "Editor" || alacritty --class Editor &
 xdo activate -N "Terminal1" || alacritty --class Terminal1 &
 xdo activate -N "Terminal2" || alacritty --class Terminal2 &       
-xdo activate -N "Google-chrome" || google-chrome-stable &
+xdo activate -N "Brave" || brave &
 xod activate -N "discord" || discord &
