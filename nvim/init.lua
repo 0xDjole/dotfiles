19502021fed5b9d78088fd0f959ac8be5e884dc4
Djole <0xdjole@gmail.com>
2023-05-24 19:51:00 +0200

    rust debug

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,7 +40,7 @@ return {
   lsp = {
     setup_handlers = {
       -- add custom handler
-      rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end
+      -- rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end
     },
     -- customize lsp formatting options
     formatting = {
